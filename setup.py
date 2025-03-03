52fe38d1d174586e49c6b2ccf2ee04092114fcce
Aster Stoustrup <33256573+aster-stoustrup@users.noreply.github.com>
2025-03-03 17:29:58 +0100

    Update setup.py with new release number

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='veropt',
-    version='0.6.0',
+    version='0.6.1',
     packages=find_packages(),
     url='https://github.com/aster-stoustrup/veropt',
     license='OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
