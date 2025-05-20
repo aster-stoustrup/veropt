882e867556df957325c5832b54ce25b817c9f8e8
aster-stoustrup <Aster.Stoustrup@gmail.com>
2025-05-20 12:52:51 +0200

    Renamed file

---
+++ examples/simple_config_testfunc_mo.py
@@ -0,0 +1,24 @@
+from veropt import BayesOptimiser
+from veropt.obj_funcs.predefined_test_functions import *
+from veropt.gui import veropt_gui
+
+n_init_points = 32
+n_bayes_points = 64
+
+# n_evals_per_step = 8
+n_evals_per_step = 4
+
+
+obj_func = PredefinedTestFunction("BraninCurrin")
+# obj_func = PredefinedTestFunction("VehicleSafety")
+
+n_objs = obj_func.n_objs
+
+optimiser = BayesOptimiser(
+    n_init_points=n_init_points,
+    n_bayes_points=n_bayes_points,
+    obj_func=obj_func,
+    n_evals_per_step=n_evals_per_step
+)
+
+veropt_gui.run(optimiser)
---
+++ veropt/obj_funcs/__init__.py
@@ -0,0 +1,17 @@
+
+from veropt.obj_funcs import ocean_sims
+from veropt.obj_funcs import predefined_test_functions
+
+from veropt.obj_funcs.ocean_sims import OceanObjFunction
+
+from veropt.obj_funcs.predefined_test_functions import FitTestFunction, PredefinedTestFunction, PredefinedFitTestFunction
+
+__all__ = [
+    "ocean_sims",
+    "predefined_test_functions.py",
+    "OceanObjFunction",
+    "FitTestFunction",
+    "PredefinedTestFunction",
+    "PredefinedFitTestFunction"
+
+]
