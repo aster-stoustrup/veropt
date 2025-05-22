960fda7f331554bf1e07a433a0743ac085da2097
aster-stoustrup <Aster.Stoustrup@gmail.com>
2025-05-22 19:23:27 +0200

    Finished making one of the structures for acq funcs

--- veropt/optimiser/acquisition.py
+++ veropt/optimiser/acquisition.py
@@ -12,7 +12,6 @@ from veropt.optimiser.optimiser_utility import get_nadir_point
 
 
 # TODO: Decide on architecture
-#   - Where does the acq func optimiser live?
 #   - How is data shared between acq func and its optimiser
 #   - How do we implement dist punish stuff
 
@@ -32,17 +31,25 @@ class AcquisitionFunction:
         return self.function(points)
 
 
-class BotorchAcquisitionFunction:
+class BotorchAcquisitionFunction(AcquisitionFunction):
 
-    def __init__(
+    @abc.abstractmethod
+    def refresh(
             self,
-            function_class: type[botorch.acquisition.AcquisitionFunction],
-            static_parameters: Optional[dict[str, float]] = None
-    ):
+            model: botorch.models.model.Model,
+            variable_values: torch.Tensor,
+            objective_values: torch.Tensor,
+    ) -> None:
+        pass
 
-        self.function_class = function_class
 
-        self.static_parameters = static_parameters or {}
+class AcquisitionBotorchqLogEHVI(BotorchAcquisitionFunction):
+
+    def __init__(self) -> None:
+
+        self.function_class = botorch.acquisition.multi_objective.logei.qLogExpectedHypervolumeImprovement
+
+        super().__init__()
 
     def refresh(
             self,
@@ -51,54 +58,21 @@ class BotorchAcquisitionFunction:
             objective_values: torch.Tensor,
     ) -> None:
 
-        dynamic_parameters = self.calculate_dynamic_parameters(
-            variable_values=variable_values,
-            objective_values=objective_values,
-        )
-
-        self.function = self.function_class(
-            model=model.get_gpytorch_model(),
-            **self.static_parameters,
-            **dynamic_parameters
-        )
-
-    @abc.abstractmethod
-    def calculate_dynamic_parameters(
-            self,
-            variable_values: torch.Tensor,
-            objective_values: torch.Tensor,
-    ) -> dict[str, Any]:
-        pass
-
-
-class AcquisitionBotorchqLogEHVI(BotorchAcquisitionFunction):
-
-    def __init__(self) -> None:
-        super().__init__(
-            function_class=botorch.acquisition.multi_objective.logei.qLogExpectedHypervolumeImprovement
-        )
-
-    def calculate_dynamic_parameters(
-            self,
-            variable_values: torch.Tensor,
-            objective_values: torch.Tensor,
-    ) -> dict[str, Any]:
-
-        dynamic_parameters = {}
-
         nadir_point = get_nadir_point(
             variable_values=variable_values,
             objective_values=objective_values
         )
 
-        dynamic_parameters['ref_point'] = nadir_point
-
-        dynamic_parameters['partitioning'] = FastNondominatedPartitioning(
+        partitioning = FastNondominatedPartitioning(
             ref_point=nadir_point,
             Y=objective_values
         )
 
-        return dynamic_parameters
+        self.function = self.function_class(
+            model=model.get_gpytorch_model(),
+            ref_point=nadir_point,
+            partitioning=partitioning
+        )
 
 
 class AcquisitionOptimiser:
