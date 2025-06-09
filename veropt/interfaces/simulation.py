e9b72c6756a7aa8c28705ba03e5841bde7dfc752
Marta Mrozowska <martamrozowska@MacBookPro.dlink>
2025-06-09 10:59:26 +0200

    Implemented saving params in a json for SimulationRunner

--- veropt/interfaces/simulation.py
+++ veropt/interfaces/simulation.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel
+import json
 from typing import Any, List, Union
 import abc
+import os
 
 # TODO: Do we stick with TypeDict throughout or can we use BaseModel?
 class SimulationResult(BaseModel):
@@ -26,30 +28,42 @@ class SimulationRunner(abc.ABC):
 
     def save_set_up_and_run(
             self,
-            id: str,
+            simulation_id: str,
             parameters: dict,
             setup_path: str,
             setup_name: str
     ) -> Union[SimulationResult, List[SimulationResult]]:
-    
-        self._save_parameters
 
-        self.set_up_and_run(
-            id=id,
+        parameters_json_filename = f"{simulation_id}_parameters.json"
+        parameters_json = os.path.join(setup_path, parameters_json_filename)
+
+        self._save_parameters(
+            parameters=parameters,
+            parameters_json=parameters_json
+        )
+
+        result = self.set_up_and_run(
+            simulation_id=simulation_id,
             parameters=parameters,
             setup_path=setup_path,
             setup_name=setup_name
         )
 
-    def _save_parameters(self):
-        # TODO: Implement
+        return result
 
-        raise NotImplementedError
+    def _save_parameters(
+            self,
+            parameters: dict,
+            parameters_json: str
+            ) -> None:
+
+        with open(parameters_json, 'w') as fp:
+            json.dump(parameters, fp)
 
     @abc.abstractmethod
     def set_up_and_run(
             self,
-            id: str,
+            simulation_id: str,
             parameters: dict,
             setup_path: str,
             setup_name: str
