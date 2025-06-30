dca6a6326312d5f77cbd951ccb18c3f10052fb87
Marta Mrozowska <martamrozowska@Martas-MacBook-Pro.local>
2025-06-30 15:31:05 +0200

    Added mock result processor for Experiment testing.

--- veropt/interfaces/result_processing.py
+++ veropt/interfaces/result_processing.py
@@ -1,12 +1,30 @@
 from abc import ABC, abstractmethod
-from typing import List
-from veropt.interfaces.simulation import SimulationResult
+from typing import Dict, Union, List
+from veropt.interfaces.simulation import SimulationResultsDict
+
+
+ObjectivesDict = Dict[int,Union[float,List[float]]]
 
 
 class ResultProcessor(ABC):
     @abstractmethod
     def process(
             self,
-            results: List[SimulationResult]
-    ) -> List[float]:
+            results: SimulationResultsDict
+    ) -> ObjectivesDict:
         ...
+
+
+class MockResultProcessor(ResultProcessor):
+    def __init__(
+            self,
+            objectives_dict: ObjectivesDict
+    ) -> None:
+        self.objectives_dict = objectives_dict
+
+    def process(
+            self,
+            results: SimulationResultsDict
+    ) -> ObjectivesDict:
+        
+        return self.objectives_dict
